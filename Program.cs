c918a92532fa941bc82bcaf522e0309ecd74ec2c
serdnago <jamonless@gmail.com>
2018-09-28 15:50:20 -1000

    Initial check-in of module Learning_Git

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Learning_Git
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //See if this works
+        }
+    }
+}
